1b0f2c5fc9948ba25b2c7eb31f5cb9bface913dc
BrodyBrosius <mcochran477@gmail.com>
2023-05-10 01:41:11 -0400

    Initial Commit

---
+++ town square/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  town square
+//
+//  Created by Mason Cochran on 5/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
