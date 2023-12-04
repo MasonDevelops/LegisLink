d9e17293bf5c12446a7f8bbc1035b2de6bc4960c
Mason Cochran <52899575+BrodyBrosius@users.noreply.github.com>
2023-12-04 09:03:34 -0500

    Delete town square directory

--- town square/View/IndivLegislationView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  IndivLegislationView.swift
-//  town square
-//
-//  Created by Mason Cochran on 11/26/23.
-//
-
-import Foundation
--- town square/View/SponsoredLegisListByPolicyAreaView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SponsoredLegisListByPolicyAreaView.swift
-//  town square
-//
-//  Created by Mason Cochran on 11/22/23.
-//
-
-import Foundation
