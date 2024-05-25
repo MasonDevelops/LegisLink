a881dcb70603b63d9d61c1c78f23f8068f758cbd
BrodyBrosius <mcochran477@gmail.com>
2024-05-25 17:31:46 -0400

    Commented out method call that was resulting in tests never finishing

--- LegisLink/ViewModel/MyRepsViewModel.swift
+++ LegisLink/ViewModel/MyRepsViewModel.swift
@@ -249,7 +249,7 @@ class MyRepsViewModel: ObservableObject {
             
             
             
-            attachCongressionalTermsInOffice()
+            //attachCongressionalTermsInOffice()
         } else {
             
             
