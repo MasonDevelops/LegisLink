07f9a6f0e6e5947d4159ad96a17cc4646490096c
BrodyBrosius <mcochran477@gmail.com>
2024-07-18 10:38:04 -0400

    Change MockOpenStatesService to use one function to get committee data for the passed-in official instead of getting it for all 3

--- Mock Services/MockOpenStatesService.swift
+++ Mock Services/MockOpenStatesService.swift
@@ -16,18 +16,40 @@ class MockOpenStatesService: OpenStatesServiceProtocol {
         return completion(2)
     }
     
-    func getSenateCommitteeData(from currentPage: Int, completion: @escaping (Swift.Result<CommitteeList, Error>) -> Void) {
-        guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-upper-house-committee-request-page-\(currentPage)", withExtension: "json"),
-              let data = try? Data(contentsOf: url) else { return }
-        let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
-        completion(.success(committeeList!))
-    }
+//    func getSenateCommitteeData(from currentPage: Int, completion: @escaping (Swift.Result<CommitteeList, Error>) -> Void) {
+//        guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-upper-house-committee-request-page-\(currentPage)", withExtension: "json"),
+//              let data = try? Data(contentsOf: url) else { return }
+//        let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
+//        completion(.success(committeeList!))
+//    }
+//    
+//    func getHouseCommitteeData(from currentPage: Int, completion: @escaping (Swift.Result<CommitteeList, Error>) -> Void) {
+//        guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-lower-house-committee-request-page-\(currentPage)", withExtension: "json"),
+//              let data = try? Data(contentsOf: url) else { return }
+//        let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
+//        completion(.success(committeeList!))
+//    }
     
-    func getHouseCommitteeData(from currentPage: Int, completion: @escaping (Swift.Result<CommitteeList, Error>) -> Void) {
-        guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-lower-house-committee-request-page-\(currentPage)", withExtension: "json"),
-              let data = try? Data(contentsOf: url) else { return }
-        let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
-        completion(.success(committeeList!))
+    
+    
+    func getCommitteeData(from chamber: String, currentPage: Int, completion: @escaping (Swift.Result<CommitteeList, Error>) -> Void) {
+        
+        if (chamber == "house") {
+            guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-lower-house-committee-request-page-\(currentPage)", withExtension: "json"),
+                  let data = try? Data(contentsOf: url) else { return }
+            let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
+            completion(.success(committeeList!))
+        }
+        
+        else {
+            guard let url = Bundle(for: MockGoogleCivicInfoService.self).url(forResource: "successful-open-states-upper-house-committee-request-page-\(currentPage)", withExtension: "json"),
+                let data = try? Data(contentsOf: url) else { return }
+            let committeeList = try? JSONDecoder().decode(CommitteeList.self, from: data)
+            completion(.success(committeeList!))
+        }
+        
+        
+
     }
     
 }
