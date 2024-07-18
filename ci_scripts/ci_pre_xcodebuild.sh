c01798deb28e6b53f13600214c9d8d9b995fe121
BrodyBrosius <mcochran477@gmail.com>
2024-07-18 15:14:30 -0400

    remove problematic line...

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -10,7 +10,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+#cd $CI_WORKSPACE/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"APP_NAME\":\"%s\",
