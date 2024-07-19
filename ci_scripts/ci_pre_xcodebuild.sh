ef3c3a4ae8132f0293a02aa31bd505242af43134
BrodyBrosius <mcochran477@gmail.com>
2024-07-18 20:22:22 -0400

    Seeing if this will add the variables...

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -14,6 +14,15 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Write a file containing all the environment variables and secrets.
 printf "APP_NAME = \"%s\"\n" "$APP_NAME" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "GOOGLE_API_KEY = \"%s\"\n" "$GOOGLE_API_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "OPEN_SECRETS_API_KEY = \"%s\"\n" "$OPEN_SECRETS_API_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "CONGRESS_GOV_API_KEY = \"%s\"\n" "$CONGRESS_GOV_API_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "OPEN_STATES_API_KEY = \"%s\"\n" "$OPEN_STATES_API_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "NYT_API_KEY = \"%s\"\n" "$NYT_API_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "AWS_REGION = \"%s\"\n" "$AWS_REGION" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "AWS_ACCESS_KEY_ID = \"%s\"\n" "$AWS_ACCESS_KEY_ID" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+printf "AWS_SECRET_ACCESS_KEY = \"%s\"\n" "$AWS_SECRET_ACCESS_KEY" >> ../Development\ Environment/DevelopmentBuildConfig.xcconfig
+
 
 echo "Wrote to xcconfig file."
 
